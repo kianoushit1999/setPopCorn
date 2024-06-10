f01d445f7d8d1bf2175c7bf0bf5e665a4cf0688b
kianoushit1999 <kianoushit1999@gmail.com>
2024-06-10 18:35:13 +0330

    split main to different components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import Nav from './NavBar/Nav';
 import { tempMovieData, tempWatchedData } from "./Data/data";
-import Main from "./Main";
+import Main from "./Main/Main";
 
 export default function App() {
   const [query, setQuery] = useState("");
